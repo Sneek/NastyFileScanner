8b3822a953efeb4ef84d318010a2eed7e64e5c75
Cristian Giordano <cristian@sneekdigital.co.uk>
2016-03-31 10:02:03 +0100

    Fixes and optimisations

--- src/Sneek/Scanner.php
+++ src/Sneek/Scanner.php
@@ -8,6 +8,8 @@ class Scanner
 {
     public  $infected_files = [];
     private $scanned_files  = [];
+    private $scan_limit;
+
     /**
      * @var Mandrill
      */
@@ -15,19 +17,20 @@ class Scanner
 
     public function __construct(Mandrill $mandrill)
     {
-        $this->mandrill = $mandrill;
+        $this->mandrill   = $mandrill;
+        $this->scan_limit = pow(10, 6);
     }
 
     public function run($dir, $to, $subject)
     {
         $this->scan($dir);
         $this->sendalert($to, $subject);
+        $this->displayResults();
     }
 
     private function scan($dir)
     {
-        $this->scanned_files[] = $dir;
-        $files                 = scandir($dir);
+        $files = scandir($dir);
 
         if ( ! is_array($files))
         {
@@ -36,13 +39,24 @@ class Scanner
 
         foreach ($files as $file)
         {
-            if (is_file($dir . '/' . $file) && ! in_array($dir . '/' . $file, $this->scanned_files))
+            if (count($this->scanned_files) > $this->scan_limit)
             {
-                $this->check(file_get_contents($dir . '/' . $file), $dir . '/' . $file);
+                $this->exitEarly();
             }
-            elseif (is_dir($dir . '/' . $file) && substr($file, 0, 1) != '.')
+
+            $itemToCheck = $dir . '/' . $file;
+
+            if (is_file($itemToCheck) and ! $this->isChecked($itemToCheck) and $this->isPhpFile($itemToCheck))
             {
-                $this->scan($dir . '/' . $file);
+                echo sprintf('[CHECKING] %s', $itemToCheck) . PHP_EOL;
+
+                $this->check(file_get_contents($itemToCheck), $itemToCheck);
+            }
+            elseif (is_dir($itemToCheck) and ! $this->isHiddenDirectory($file) and ! $this->isNodeModulesDirectory($file))
+            {
+                echo sprintf('[MOVING] %s', $itemToCheck) . PHP_EOL;
+
+                $this->scan($itemToCheck);
             }
         }
     }
@@ -68,14 +82,75 @@ class Scanner
             }
 
             $this->mandrill->messages->send([
-                'text' => $message,
+                'text'       => $message,
                 'from_email' => 'developers@sneekdigital.co.uk',
-                'from_name' => 'Sneek Digital',
-                'subject' => $subject,
-                'to'   => [
+                'from_name'  => 'Sneek Digital',
+                'subject'    => $subject,
+                'to'         => [
                     ['email' => $to]
                 ]
             ]);
         }
+    }
+
+    /**
+     * @param $file
+     *
+     * @return bool
+     */
+    private function isHiddenDirectory($file)
+    {
+        return substr($file, 0, 1) == '.';
+    }
+
+    private function isNodeModulesDirectory($file)
+    {
+        return substr($file, mb_strlen('node_modules') * -1) === 'node_modules';
+    }
+
+    /**
+     * @param $itemToCheck
+     *
+     * @return bool
+     */
+    private function isChecked($itemToCheck)
+    {
+        return in_array($itemToCheck, $this->scanned_files);
+    }
+
+    private function isPhpFile($itemToCheck)
+    {
+        return substr($itemToCheck, - 3) === 'php';
+    }
+
+    private function exitEarly()
+    {
+        $this->displayResults();
+        exit;
+    }
+
+    private function getMemoryUsage()
+    {
+        $mem   = memory_get_peak_usage(true);
+        $unit  = 0;
+        $units = ['B', 'KB', 'MB', 'GB', 'TB', 'ZB'];
+
+        while ($mem >= 1024)
+        {
+            $mem /= 1024;
+            $unit ++;
+        }
+
+        return $mem . $units[$unit];
+    }
+
+    private function displayResults()
+    {
+        $memoryUsage = $this->getMemoryUsage();
+
+        echo '== Usage ==' . PHP_EOL;
+        echo 'Memory Usage: ' . $memoryUsage . PHP_EOL;
+        echo 'Scanned Files: ' . count($this->scanned_files) . PHP_EOL;
+        echo '================' . PHP_EOL;
     }
 }
